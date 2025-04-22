47e65e4fcf6ccbacbdc6ac745594344c757e8443
Te Piha <54650178+Te-Piha@users.noreply.github.com>
2025-04-23 11:17:19 +1200

    update bots for testing

--- bots/bot1.js
+++ bots/bot1.js
@@ -1,18 +1,8 @@
-
-export default function Bot2(history, self, opponent) {
-    // Basic logic: if opponent has shot last round, block; else reload or shoot randomly
-    const lastRound = history[history.length - 1];
-    if (lastRound) {
-      const opponentLast = lastRound.results.find(r => r.name === opponent.name);
-      if (opponentLast?.choice === "Shoot") {
-        return "Block";
-      }
-    }
-  
-    // Random between reload or shoot if you have ammo
-    if (self.ammo > 0 && Math.random() > 0.5) {
+export default function Bot1(history, self, opponent) {
+    // Basic logic: shoot if you have ammo, else reload
+    if (self.ammo > 0) {
       return "Shoot";
+    } else {
+      return "Reload";
     }
-    return "Reload";
   }
-  
