f995b74244182499d6d8c54c908ca1c1fb4c48e4
William <w.moore@ngenroom.co.nz>
2025-10-05 17:03:15 +1300

    changed to a 50 / 50 strat

--- bots/clank.js
+++ bots/clank.js
@@ -1,13 +1,8 @@
 export default function Clank(history, self, opponent) {
+    if (self.ammo === 0) return "Reload";
+    
     if (opponent.ammo >= 1) {
-      return "Block";
+        return Math.random() < 0.5 ? "Block" : "Shoot";
     }
-     else if (self.ammo == 0){
-      return "Reload";
-    }   
-  }
-if (opponent.ammo == 0) {
-if (self.ammo > 1) {
-return "Shoot";  }
-
+    return Math.random() < 0.5 ? "Shoot" : "Reload";
 }
